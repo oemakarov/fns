0a43284352114fb72f9d3f276f3e0cb16ff692b6
oemakarov <oemakarov@gmail.com>
2023-01-16 15:49:29 +0300

    v0.23 -  +fl_passport_prepare

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
-__version__ = '0.23'
-
 from __version__ import __version__
 from .fns import FNS
 from .fl import (
@@ -7,10 +5,3 @@ from .fl import (
     find_fl_inn_new,
     fl_passport_prepare,
 )
-
-
-# v0.23
-# - метод для подготовки паспортных данных к запросу на ИНН ФЛ fl_passport_prepare
-
-# v0.22
-# - рефакторинг 
